2fe5c5937b154a1cecfc5c3fde081d99576e791a
as-chapa <e.goromgorom@gmail.com>
2020-05-04 07:24:57 +0900

    chap5 done

--- chap5/test_money.py
+++ chap5/test_money.py
@@ -1,8 +1,9 @@
 import unittest
 # テストコードを別ディレクトリにする場合はここのimport指定を変える
 import dollar
+import franc
 
-class TestDollar(unittest.TestCase):
+class TestMoney(unittest.TestCase):
     def test_multiplication(self):
         five = dollar.Dollar(5)
         # Pythonのインスタンスの等価性比較は、比較対象を明示しないといけないので、amountで比較
@@ -13,6 +14,13 @@ class TestDollar(unittest.TestCase):
     def test_equality(self):
         self.assertTrue(dollar.Dollar(5).equals(dollar.Dollar(5)))
         self.assertFalse(dollar.Dollar(5).equals(dollar.Dollar(6)))
+
+    def test_franc_multiplication(self):
+        five = franc.Franc(5)
+        # Pythonのインスタンスの等価性比較は、比較対象を明示しないといけないので、amountで比較
+        # ただ、これって test_equality と同じことになっている気が。
+        self.assertEqual(franc.Franc(10).amount,five.times(2).amount)
+        self.assertEqual(franc.Franc(15).amount,five.times(3).amount)
 
 if __name__ == '__main__':
     unittest.main()
