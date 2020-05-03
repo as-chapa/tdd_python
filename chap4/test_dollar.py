62676d56278b1201ef4081b6511dcc1e8614f8be
as-chapa <e.goromgorom@gmail.com>
2020-05-04 06:34:00 +0900

    chap4 done

--- chap4/test_dollar.py
+++ chap4/test_dollar.py
@@ -5,10 +5,10 @@ import dollar
 class TestDollar(unittest.TestCase):
     def test_multiplication(self):
         five = dollar.Dollar(5)
-        product = five.times(2)
-        self.assertEqual(10,product.amount)
-        product = five.times(3)
-        self.assertEqual(15,product.amount)
+        # Pythonのインスタンスの等価性比較は、比較対象を明示しないといけないので、amountで比較
+        # ただ、これって test_equality と同じことになっている気が。
+        self.assertEqual(dollar.Dollar(10).amount,five.times(2).amount)
+        self.assertEqual(dollar.Dollar(15).amount,five.times(3).amount)
     
     def test_equality(self):
         self.assertTrue(dollar.Dollar(5).equals(dollar.Dollar(5)))
