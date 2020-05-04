ea9831bd04b4fb239b96a94c125e83f9e25f23ee
as-chapa <e.goromgorom@gmail.com>
2020-05-05 06:00:07 +0900

    add money.py

--- chap6/dollar.py
+++ chap6/dollar.py
@@ -1,12 +1,12 @@
-class Dollar():
-    __amount:int
+import money
 
+class Dollar(money.Money):
     def __init__(self,amount:int):
-        self.__amount = amount
+        self._amount = amount
 
     @property
     def amount(self):
-        return self.__amount
+        return self._amount
 
     def times(self,multiplier:int):
         return(Dollar(self.amount * multiplier))
---
+++ chap6/money.py
@@ -0,0 +1,3 @@
+class Money():
+    # pythonにはprotectedがないので、慣習的に「_」で定義する
+    _amount : int
