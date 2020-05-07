8b77c4d747d9ec03afa4e990641f29b040ea3b05
as-chapa <e.goromgorom@gmail.com>
2020-05-08 05:56:52 +0900

    chap9 done

--- source/money.py
+++ source/money.py
@@ -3,12 +3,19 @@ from abc import ABCMeta, abstractmethod
 class Money(metaclass=ABCMeta):
     # pythonにはprotectedがないので、慣習的に「_」で定義する
     _amount : int
-    def __init__(self,amount:int):
+    _currency : str
+    def __init__(self,amount:int, currency:str):
         self._amount = amount
+        self._currency = currency
 
+    # propetyデコレータで外部から直接変更しにくくする（言語特性上、禁止はできない）
     @property
     def amount(self):
         return self._amount
+
+    @property
+    def currency(self):
+        return self._currency
 
     @abstractmethod
     def times(self,multiplier:int):
@@ -20,17 +27,16 @@ class Money(metaclass=ABCMeta):
 
     @staticmethod
     def Dollar(amount: int):
-        return Dollar(amount)
+        return Dollar(amount, 'USD')
 
     @staticmethod
     def Franc(amount: int):
-        return Franc(amount)
+        return Franc(amount, 'CHF')
 
 class Dollar(Money):
     def times(self,multiplier:int) -> Money:
-        return(Dollar(self.amount * multiplier))
+        return(Money.Dollar(self.amount * multiplier))
 
 class Franc(Money):
     def times(self,multiplier:int) -> Money:
-        return(Franc(self.amount * multiplier))
-
+        return(Money.Franc(self.amount * multiplier))
--- source/test_money.py
+++ source/test_money.py
@@ -5,8 +5,6 @@ from money import Money
 class TestMoney(unittest.TestCase):
     def test_multiplication(self):
         five: Money = Money.Dollar(5)
-        # Pythonのインスタンスの等価性比較は、比較対象を明示しないといけないので、amountで比較
-        # ただ、これって test_equality と同じことになっている気が。
         self.assertEqual(Money.Dollar(10).amount,five.times(2).amount)
         self.assertEqual(Money.Dollar(15).amount,five.times(3).amount)
     
@@ -19,10 +17,12 @@ class TestMoney(unittest.TestCase):
 
     def test_franc_multiplication(self):
         five = Money.Franc(5)
-        # Pythonのインスタンスの等価性比較は、比較対象を明示しないといけないので、amountで比較
-        # ただ、これって test_equality と同じことになっている気が。
         self.assertEqual(Money.Franc(10).amount,five.times(2).amount)
         self.assertEqual(Money.Franc(15).amount,five.times(3).amount)
+
+    def test_currency(self):
+        self.assertEqual('USD',Money.Dollar(1).currency)
+        self.assertEqual('CHF',Money.Franc(1).currency)
 
 if __name__ == '__main__':
     unittest.main()
